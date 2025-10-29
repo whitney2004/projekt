464dd8f160511bb64717811740473bd364d05253
wxkhfs6f5j-beep <karolina.korvin@gmail.com>
2025-10-29 13:14:28 +0100

    commit3
    paar neue änderungen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("Have a nice day"); //zweite sout
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5800; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("i = " + i + i);
         }
     }
-}
+} //Kommentar
